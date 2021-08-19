0036713428e84014e3104dafef099b44bb1ac2a3
alleytopcat <donmaoccio@icloud.com>
2021-08-18 19:39:54 -0500

    Dibujar Tablero

---
+++ TicTacToe/TicTacToe/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        // Crear arreglo bidimensional
+        static int[,] tablero = new int[3, 3]; // 3 filas 3 columnas
+        // crear un arreglo parasimbolos del tablero
+        static char[] simbolo = { ' ', 'O', 'X' };
+
+        static void Main(string[] args)
+        {
+
+            // Dibuja el tablero
+            DibujarTablero();
+        } // Cierre de MAIN
+        static void DibujarTablero()
+        {
+            // Variables de conteo
+            int fila = 0;
+            int columna = 0;
+
+            Console.WriteLine(); // Margen del tablero
+            Console.WriteLine("-------------");
+        }
+    }
+}
